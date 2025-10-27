b50d335d9dc489ae18935f9f93c884dc9d0c26e0
Tormey Reimer <tormey.reimer@utas.edu.au>
2025-10-28 10:07:14 +1100

    Conflicts

---
+++ R_scripts/00_functions.R
@@ -0,0 +1,267 @@
+# This script includes various helper functions that are used across all parts of the project
+suppressMessages(suppressWarnings({
+  library(tidyr)
+  library(dplyr)
+  library(units)
+  library(qs)
+  library(qs2)
+  library(magrittr)
+  library(stringr)
+  library(raster)
+  library(sp)
+  library(units)
+  library(streamMetabolizer)
+  library(lubridate)
+}))
+
+fixnum <- function(n, digits = 4) {
+  vapply(n, function(x) {
+    stringr::str_flatten(c(rep("0", digits-nchar(as.character(x))), as.character(x)))
+  }, character(1))
+}
+
+sumna <- function(x) {sum(x, na.rm = T)}
+maxna <- function(x) {max(x, na.rm = T)}
+minna <- function(x) {min(x, na.rm = T)}
+meanna <- function(x) {mean(x, na.rm = T)}
+medna <- function(x) {median(x, na.rm = T)}
+sdna <- function(x) {sd(x, na.rm = T)}
+
+cite_pack <- function(package_name) {
+  version <- packageVersion(package_name)
+  paste0("package `", package_name, "` version ", version, " [@", package_name, "]")
+}
+
+cite_packages <- function(packages) {
+  # Generate citations for each package
+  citations <- sapply(packages, function(pkg) {
+    version <- packageVersion(pkg)
+    paste0("`", pkg, "`, version ", version, " [@", pkg, "]")
+  })
+  
+  # Handle different cases based on number of packages
+  if (length(packages) == 1) {
+    return(paste0("package ", citations[1]))
+  } else if (length(packages) == 2) {
+    return(paste0("packages ", citations[1], " and ", citations[2]))
+  } else {
+    # More than 2 packages: use commas and "and" before last
+    first_part <- paste(citations[-length(citations)], collapse = ", ")
+    last_part <- citations[length(citations)]
+    return(paste0("packages ", first_part, ", and ", last_part))
+  }
+}
+
+
+adj_params <- function(params, focus_param, factor) {
+  params[focus_param] <- params[focus_param] * factor
+  return(params)
+}
+
+buffer_cells <- function(cell_coords, coastline, dist) {
+  cell_coords$in_buffer <- NA
+  
+  for (i in 1:nrow(cell_coords)) {
+    tmp <- geosphere::distHaversine(
+      p1 = as.matrix(coastline), 
+      p2 = cbind(cell_coords$lon[i], cell_coords$lat[i])
+    ) * 10^-3
+    if(any(tmp <= dist)) {
+      cell_coords$in_buffer[i] <- 1
+    }
+  }
+  return(cell_coords)
+}
+
+define_coastline <- function(coast_data) {
+  statelist <- list()
+  for (i in 1:length(coast_data)) {
+    statelist[[i]] <- as.data.frame(coast_data[[i]][[1]])
+  }
+  coast_data <- statelist %>% 
+    bind_rows()
+  return(coast_data)
+}
+
+positive_end <- function(df) {
+  df <- df %>% 
+    filter(output %in% c("Nf", "Ns")) %>% 
+    mutate(plant_date = min(date)) %>% 
+    rename(harv_date = date) %>% 
+    group_by(plant_date, harv_date, state, cell_ID, species, batch) %>% 
+    reframe(value = sum(value))
+  df <- df[df$value == max(df$value), ]
+  return(df)
+}
+
+get_total_N_long <- function(df) {
+  df <- df[df$output %in% c("Nf", "Ns"), ] 
+  df <- dplyr::group_by(df, across(all_of(colnames(df)[!colnames(df) %in% c("output", "value")]))) 
+  df <- dplyr::reframe(df, total_N = sum(value, na.rm = F))
+  return(df)
+}
+
+map_cells <- function(cell_df, data_df, try_dist) {
+  # Distance of the data cell to every map cell centre
+  dist <- geosphere::distHaversine(
+    p1 = c(cell_df$lon, cell_df$lat), 
+    p2 = as.matrix(cbind(data_df$lon, data_df$lat))
+  ) * 10^-3
+  dist <- dist[which(dist <= max(try_dist))]
+  
+  if (any(dist <= try_dist[1])) {
+    dat <- data.frame(data_cell_ID = data_df$cell_ID[which(dist <= try_dist[1])],
+                      dist = dist[which(dist <= try_dist[1])])
+  } else if (any(dist <= try_dist[2])) {
+    dat <- data.frame(data_cell_ID = data_df$cell_ID[which(dist <= try_dist[2])],
+                      dist = dist[which(dist <= try_dist[2])])
+  } else if (any(dist <= try_dist[3])) {
+    dat <- data.frame(data_cell_ID = data_df$cell_ID[which(dist <= try_dist[3])],
+                      dist = dist[which(dist <= try_dist[3])])
+  } else if (any(dist <= try_dist[4])) {
+    dat <- data.frame(data_cell_ID = data_df$cell_ID[which(dist <= try_dist[4])],
+                      dist = dist[which(dist <= try_dist[4])])
+  } else {
+    dat <- data.frame(data_cell_ID = NA,
+                      dist = NA)
+  }
+  dat$map_cell_ID <- cell_df$cell_ID
+  
+  return(dat)
+}
+
+match_location <- function(data_coords, cell_coords, choose = 2) {
+  data_coords$dist <- geosphere::distHaversine(
+    p = c(cell_coords$lon, cell_coords$lat), 
+    p2 = as.matrix(cbind(data_coords$lon, data_coords$lat))
+  )
+  if (!is.na(choose)) {
+    data_coords <- arrange(data_coords, dist) %>% slice_head(n = choose)
+  }
+  return(data_coords)
+}
+
+combine_outfall_data <- function(conc_df, vol_df, dist_df) {
+  if (nrow(conc_df) != 0 & nrow(vol_df) != 0) {
+    df <- merge(conc_df, vol_df, by = c("name", "indicator", "month", "cell_ID"), all = T) %>%
+      mutate(value = outfall_conc_mgm3 * prop_vol) %>% 
+      dplyr::select(-c(outfall_conc_mgm3, prop_vol)) %>% 
+      merge(dist_df, by = c("cell_ID", "name"))
+  } else {
+    df <- dist_df %>% 
+      mutate(indicator = NA, month = NA, value = NA) %>% 
+      relocate(dist, .after = value) %>%
+      relocate(cell_ID, .before = name)
+    colnames(df) <- c("cell_ID", "name", "indicator", "month", "value", "dist")
+  }
+  return(df)
+}
+
+consecutive <- function(df, measure, threshold) {
+  colnames(df) <- colnames(df) %>% stringr::str_replace(measure, "result")
+  df <- df %>% 
+    mutate(thresh_met = case_when(result >= threshold ~ 1, TRUE ~ 0),
+           consec = case_when(t == 1 & thresh_met == 1 ~ 1, TRUE ~ 0))
+  
+  for (i in 2:nrow(df)) {
+    if (df$thresh_met[i] == 1 & df$thresh_met[i-1] == 1) {
+      df$consec[i] <- df$consec[i-1] + 1
+    } 
+  }
+  return(df)
+}
+
+# Function to fill NAs with weighted averages
+fill_nas_weighted <- function(x, window_size = 8, decay_factor = 0.65) {
+  n <- length(x)
+  result <- x
+  
+  # Find positions of NAs
+  na_positions <- which(is.na(x))
+  
+  for (i in na_positions) {
+    # Define window range around missing value
+    start_idx <- max(1, i - window_size)
+    end_idx <- min(n, i + window_size)
+    
+    # Get non-NA values in window
+    window_values <- x[start_idx:end_idx]
+    window_positions <- start_idx:end_idx
+    valid_indices <- which(!is.na(window_values))
+    
+    if (length(valid_indices) > 0) {
+      valid_values <- window_values[valid_indices]
+      valid_positions <- window_positions[valid_indices]
+      
+      # Calculate distances from the NA value
+      distances <- abs(valid_positions - i)
+      
+      # Calculate weights based on distances (closer values have higher weights)
+      weights <- decay_factor^distances
+      
+      # Calculate weighted average
+      result[i] <- sum(valid_values * weights) / sum(weights)
+    }
+  }
+  
+  # Any remaining NAs (e.g., if entire window was NA)
+  still_na <- which(is.na(result))
+  if (length(still_na) > 0) {
+    # Use global mean for any remaining NAs
+    result[still_na] <- mean(x, na.rm = TRUE)
+  }
+  
+  return(result)
+}
+
+fill_raster_gaps <- function(raster, cell, output) {
+  if (any(is.na(output))) {
+    neighbours <- terra::adjacent(raster, cell, "8")[1,]
+    output <- terra::extract(raster, neighbours) %>% colMeans(na.rm = T)
+  }
+  if (any(is.na(output))) {
+    neighbours <- terra::adjacent(raster, cell, "16")[1,]
+    output <- terra::extract(raster, neighbours) %>% colMeans(na.rm = T)
+  }
+  if (any(is.na(output))) {
+    neighbours <- terra::adjacent(raster, neighbours, "16") %>% as.vector() %>% unique()
+    neighbours <- neighbours[!is.na(neighbours)]
+    output <- terra::extract(raster, neighbours) %>% colMeans()
+  }
+  return(unname(output))
+}
+
+# raster <- BRAN_salt_raster
+# point <- c(cell_coords$lon, cell_coords$lat)
+
+extract_stepwise_buffer <- function(raster, point, distances = c(1000, 5000, 10000, 25000, 50000, 75000)) {
+  # Get layer names for output
+  layer_names <- names(raster)
+  
+  # Try exact point first
+  point_vect <- terra::vect(matrix(point, ncol = 2), crs = terra::crs(raster))
+  exact_values <- terra::extract(raster, point_vect, method = "simple", ID = F) %>% 
+    unlist() %>% unname()
+  
+  if (all(is.na(exact_values))) {
+    # Try each distance buffer
+    for (dist in distances) {
+      buffer_poly <- terra::buffer(point_vect, width = dist)
+      buffered_values <- terra::extract(raster, buffer_poly, method = "simple", ID = F)
+      
+      # Calculate mean for each layer, excluding NAs
+      layer_means <- numeric(terra::nlyr(raster))
+      names(layer_means) <- layer_names
+      
+      for (i in seq_along(layer_names)) {
+        layer_means[i] <- mean(buffered_values[, i], na.rm = TRUE)
+      }
+      values <- unname(layer_means)
+
+    if (!all(is.na(values))) {
+        return(values)
+      }
+    }
+  }
+  return(exact_values)
+}
--- _all_projects_RUN.R
+++ _all_projects_RUN.R
@@ -1,92 +1,131 @@
-library(targets, warn.conflicts = F)
-library(magrittr, warn.conflicts = F)
-library(stringr, warn.conflicts = F)
-library(dplyr, warn.conflicts = F)
-library(gitcreds, warn.conflicts = F)
-fixnum <- function(n, digits = 4) {
-  vapply(n, function(x) {
-    str_flatten(c(rep("0", digits-nchar(as.character(x))), as.character(x)))
-  }, character(1))
-}
+# Setup -----------------------------------------------------------------------------------------------------------
+library(targets)
+library(magrittr)
+library(stringr)
+library(dplyr)
+library(gitcreds)
+library(here)
+library(macrogrow)
+library(ggplot2)
+
+here() %>% file.path("R_scripts", "00_functions.R") %>% source()
 
 # Species ---------------------------------------------------------------------------------------------------------
-# The species project collates all the species data into iterable lists for easier model running, and also runs all the sensitivity analyses on species parameters. It doesn't use anything from the other targets projects.
-  # - parameterisation/asparagopsis-parameterisation.qmd
+# The species markdown details the parameterisation process for _Asparagopsis armata_ and _Asparagopsis taxiformis_. 
+# It collates all the species parameters into named number vectors for easier model running, and also outputs them 
+# into human-readable .csv files in the "data" folder. 
+  # terminal: quarto render parameterisation/asparagopsis-parameterisation.qmd
 
-## Species targets ------------------------------------------------------------------------------------------------
+# The project_species targets pipeline collates the relevant species data into iterable lists for easier model 
+# running later on, and also runs all the sensitivity analyses on species parameters. 
 Sys.setenv(TAR_PROJECT = "project_species")
+tar_make(
+  explore_starting_biomass,
+  seconds_meta_append = 300,
+  callr_function = NULL
+)
+tar_load(explore_starting_biomass)
+explore_starting_biomass %>% 
+  ggplot(aes(x = Nf, y = N_rem)) +
+  geom_line()
+tar_prune()
 
-tar_outdated()
-tar_make(seconds_meta_append = 300)
+tar_make(
+  seconds_meta_append = 300,
+  callr_function = NULL
+)
 source("R_scripts/03_processing-species.R")
 
-# Spatial cells data ----------------------------------------------------------------------------------------------
-# The spatial-cells project processes the more ad-hoc data for nitrogen, and collates all the environmental data 
+# Environmental data ----------------------------------------------------------------------------------------------
+# The spatial-cells project processes the ad-hoc data for nitrogen, and collates all the environmental data into 
+# discrete cell-specific (and state-specific) objects that are easier to call for actual runs.
+
+# Running these scripts requires that the user has downloaded the relevant raw data into the following path:
+big_path <- file.path("D:", "FRDC-Seaweed-Raw-Data")
+
+load(file.path("D:", "FRDC-Seaweed-Raw-Data", "BARRA2", "BARRA2_cell_coords.Rdata")) # cell_coords for BARRA
+
+# BARRA2 data was downloaded from Thredds: 
+#   https://geonetwork.nci.org.au/geonetwork/srv/eng/catalog.search#/metadata/f9057_2475_0540_0329
+# It requires access to the ob53 project.
+raw_data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "BARRA-R2")
+final_data_path <- here() %>% file.path("data_raw", "BARRA-R2")
 overwrite <- F
-
-# into discrete cell-specific (and state-specific) objects that are easier to call for actual runs.
-load(file.path("D:", "FRDC-Seaweed-Raw-Data", "BARRA-C2", "BARRA_C2_cell_coords.Rdata")) # cell_coords for BARRA
-cell_sample <- NA # cell_coords %>% 
-  # filter(layer <= 97.5) %>% 
-  # slice_sample(n = 2500)
-
-# BARRA2-R data was downloaded from: [Thredds](https://thredds.nci.org.au/thredds/fileServer/ob53/output/reanalysis/AUS-11/BOM/ERA5/historical/hres/BARRA-R2/v1/day/catalog.html). It requires access to the ob53 project.
-data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "BARRA2")
-source("R_scripts/04_extract-BARRA2-data.R")
-
-# BARRA-C2 data was downloaded from: [Thredds](https://thredds.nci.org.au/thredds/catalog/ob53/output/reanalysis/AUST-04/BOM/ERA5/historical/hres/BARRA-C2/v1/day/catalog.html). It requires access to the ob53 project.
-data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "BARRA-C2")
 source("R_scripts/05_extract-BARRA-C2-data.R")
 
 # BRAN2023 data was downloaded from: [Thredds](https://thredds.nci.org.au/thredds/catalog/gb6/BRAN/BRAN2023/daily/catalog.html). It requires access to the gb6 project.
-data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "BRAN2023")
-source("R_scripts/06_extract-BRAN2023-data.R")
+raw_data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "BRAN2023")
+source("R_scripts/05_extract-BRAN2023-data.R")
 
+# Aqua MODIS data was downloaded as png files from: [MODIS NASA](http://oceancolor.gsfc.nasa.gov/l3)
 data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "Aqua_MODIS_KD")
-source("R_scripts/07_extract-MODIS-data.R")
+source("R_scripts/06_extract-MODIS-data.R")
 
 # AusBathyTopo 2023 data was downloaded from: [Metadata catalogue](https://doi.org/10.26186/148758).
 data_path <- file.path("D:", "FRDC-Seaweed-Raw-Data", "AusBathyTopo 2024")
-source("R_scripts/08_extract-AusBathyTopo-data.R")
+source("R_scripts/07_extract-AusBathyTopo-data.R")
 
 ## Spatial data targets -------------------------------------------------------------------------------------------
 Sys.setenv(TAR_PROJECT = "project_inputs")
 
 tar_outdated()
-tar_make(seconds_meta_append = 90)
-source("R_scripts/10_processing_env_inputs.R")
+tar_make(
+  seconds_meta_append = 120,
+  callr_function = NULL
+)
+tar_prune()
+source("R_scripts/09_processing_env_inputs.R")
 
 # Model running ---------------------------------------------------------------------------------------------------
 Sys.setenv(TAR_PROJECT = "project_running")
 
 tar_outdated()
-tar_make(seconds_meta_append = 190)
-source("R_scripts/12_processing_model_running.R")
+
+# Quicker test
+tar_invalidate("test_growth_armata")
+tar_make(test_growth_armata)
+tar_load(test_growth_armata)
+ggplot(test_growth_armata, aes(x = t, y = B_dw.mg, color = as.factor(start))) +
+  geom_line(linewidth = 0.75) +
+  facet_grid(rows = vars(state)) +
+  theme_classic()
+ggplot(test_growth_armata, aes(x = t, y = (Nf+Ns)*hm, color = as.factor(start))) +
+  geom_line(linewidth = 0.75) +
+  facet_grid(rows = vars(state)) +
+  theme_classic()
+
+tar_make(
+  seconds_meta_append = 90,
+  callr_function = NULL
+)
+tar_prune()
+
+source("R_scripts/11_processing_model_running.R")
 
 # Renv files ------------------------------------------------------------------------------------------------------
 library(gitcreds)
 library(magrittr)
 
 targets::tar_renv(
-  extras = c("bslib", "crew", "gt", "markdown", "rstudioapi", "shiny", "shinybusy", "shinyWidgets", "visNetwork", "qs", "qs2"),
-  path = file.path("renv", "09_env_inputs.R"),
-  script = file.path("R_scripts", "09_env_inputs.R")
+  extras = c("bslib", "crew", "gt", "markdown", "rstudioapi", "shiny", "shinybusy", "shinyWidgets", "visNetwork", "qs", "qs2", "rbenchmark"),
+  path = file.path("renv", "packages_inputs.R"),
+  script = file.path("targets_pipelines", "env_inputs.R")
 )
 
 targets::tar_renv(
   path = file.path("renv", "packages_species.R"),
-  script = file.path("R_scripts", "02_species.R")
+  script = file.path("targets_pipelines", "species.R")
 )
 
 targets::tar_renv(
   path = file.path("renv", "packages_running.R"),
-  script = file.path("R_scripts", "11_model_running.R")
+  script = file.path("targets_pipelines", "model_running.R")
 )
 
 packs <- renv::dependencies()$Package %>% unique()
 renv::install(
   packages = c(packs, "stormeyseas/macrogrow", "DOI-USGS/streamMetabolizer", "markwh/subsetnc", "ropensci/geotargets"),
-  exclude = c("macrogrow", "streamMetabolizer", "subsetnc", "geotargets"),
+  exclude = c("macrogrow", "streamMetabolizer", "subsetnc", "geotargets", "RPostgreSQL"),
   dependencies = T
 )
 renv::snapshot()
